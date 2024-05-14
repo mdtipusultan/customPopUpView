44b11f9340b0c6927b3d95eef0b8cd163a47b2a9
Tipu-leads <tipu.diu.cse@gmail.com>
2024-05-14 11:12:12 +0600

    initial commit to add project in folder
    initial commit to add project in folder

---
+++ customPopUp/customPopUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  customPopUp
+//
+//  Created by LEADS Corporation Limited on 14/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
