e6cab1517ec9ba58e21d1e633a090a9769121dd7
Tipu-leads <tipu.diu.cse@gmail.com>
2024-05-14 11:27:40 +0600

    cutompopup added
    cutompopup added

--- customPopUp/customPopUp/ViewController.swift
+++ customPopUp/customPopUp/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        //OverLayerView.shared.appear(sender: self)
+        let overLayerView = OverLayerView()
+        overLayerView.appear(sender: self)
+    }
 
 }
 
